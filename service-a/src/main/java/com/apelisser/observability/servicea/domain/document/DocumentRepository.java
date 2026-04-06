f80cb58c33851664f1594c5cb1a03f5439bcb9c2
Abner J Pelisser <abner.pelisser@gmail.com>
2026-04-06 00:27:37 -0300

    chore: remove unused projects and lgtm configs

--- service-a/src/main/java/com/apelisser/observability/servicea/domain/document/DocumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apelisser.observability.servicea.domain.document;
-
-import java.util.UUID;
-
-public interface DocumentRepository {
-
-    UUID save(Document document);
-
-}
--- service-a/src/main/java/com/apelisser/observability/servicea/domain/document/DocumentStorage.java
+++
@@ -1,9 +0,0 @@
-package com.apelisser.observability.servicea.domain.document;
-
-import java.io.InputStream;
-
-public interface DocumentStorage {
-
-    void save(String filename, InputStream content);
-
-}
