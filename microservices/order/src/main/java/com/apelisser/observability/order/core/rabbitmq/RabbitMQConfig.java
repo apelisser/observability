9f10967a486a7f117f22e2dc936278f76e4894e2
apelisser <abner.pelisser@gmail.com>
2026-04-16 00:29:21 -0300

    feat(order): add a notification publisher for rabbitmq

---
+++ microservices/order/src/main/java/com/apelisser/observability/order/core/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,40 @@
+package com.apelisser.observability.order.core.rabbitmq;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String EXCHANGE = "notification.events";
+
+    public static final List<String> ROUTING_KEYS = List.of(
+        "notification.email.send",
+        "notification.sms.send"
+    );
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
+
+}
--- microservices/order/src/main/java/com/apelisser/observability/order/service/NotificationService.java
+++ microservices/order/src/main/java/com/apelisser/observability/order/service/NotificationService.java
@@ -1,21 +1,42 @@
 package com.apelisser.observability.order.service;
 
+import com.apelisser.observability.order.core.rabbitmq.RabbitMQConfig;
 import com.apelisser.observability.order.model.Notification;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static com.apelisser.observability.order.core.rabbitmq.RabbitMQConfig.*;
 
 @Slf4j
 @Service
 public class NotificationService {
 
-    private final Semaphore limit = new Semaphore(2);
+    private final Semaphore limit = new Semaphore(4);
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public NotificationService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
     public void notify(Notification notification) {
-        // TODO
         log.info("Sending notification: {}", notification);
+        String routingKey = getRoutingKey(notification);
+        setNotification(routingKey, notification);
+    }
+
+    private void setNotification(String routingKey, Notification notification) {
+        rabbitTemplate.convertAndSend(EXCHANGE, routingKey, notification);
+    }
+
+    private String getRoutingKey(Notification notification) {
+        int routingKey = ThreadLocalRandom.current().nextInt(0, ROUTING_KEYS.size());
+        return ROUTING_KEYS.get(routingKey);
     }
 
     @Async
@@ -27,6 +48,8 @@ public class NotificationService {
 
             // TODO
             log.info("Sending async notification: {}", notification);
+            String routingKey = getRoutingKey(notification);
+            setNotification(routingKey, notification);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             log.error("Interrupted while waiting for notification", e);
