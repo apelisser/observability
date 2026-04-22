075866947344af8d98ef1dbbf610e0e2c683e6a8
apelisser <abner.pelisser@gmail.com>
2026-04-22 00:23:22 -0300

    feat(order): enable trace context propagation for rabbitmq publisher

--- microservices/order/src/main/java/com/apelisser/observability/order/core/rabbitmq/RabbitMQConfig.java
+++ microservices/order/src/main/java/com/apelisser/observability/order/core/rabbitmq/RabbitMQConfig.java
@@ -5,8 +5,10 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.amqp.autoconfigure.RabbitTemplateConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.util.List;
 
@@ -30,9 +32,32 @@ public class RabbitMQConfig {
         return new JacksonJsonMessageConverter();
     }
 
+    /**
+     * Customiza o RabbitTemplate com suporte a propagação de trace (observation-enabled).
+     * ATENÇÃO: este bean ignora as configurações definidas em application.properties/yaml
+     * (spring.rabbitmq.template.*). Qualquer propriedade necessária deve ser configurada
+     * explicitamente aqui. Prefira usar RabbitTemplateConfigurer para herdar as configs do Spring Boot.
+     */
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+    public RabbitTemplate rabbitTemplateManual(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setObservationEnabled(true);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
+
+    /**
+     * Customiza o RabbitTemplate mantendo todas as configurações definidas em application.properties/yaml
+     * (ex: spring.rabbitmq.template.*), incluindo observation-enabled para propagação de trace.
+     * O uso do RabbitTemplateConfigurer garante que as propriedades do Spring Boot sejam aplicadas
+     * antes das customizações adicionais deste bean.
+     */
+    @Bean
+    @Primary
+    public RabbitTemplate rabbitTemplateConfigured(RabbitTemplateConfigurer configurer, ConnectionFactory connectionFactory,
+            MessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate();
+        configurer.configure(template, connectionFactory);
         template.setMessageConverter(messageConverter);
         return template;
     }
